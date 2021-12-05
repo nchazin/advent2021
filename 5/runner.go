3a11e443e3b44d3d2613984c6abf2020b11ee15d
Neil Chazin <nchazin@salesforce.com>
2021-12-04 23:37:47 -0800

    Day 5 Part 2

--- 5/runner.go
+++ 5/runner.go
@@ -14,7 +14,36 @@ type Point struct {
 	y int
 }
 
-func fill_field(field map[Point]int, a Point, b Point) {
+func fill_field_diagonal(field map[Point]int, a Point, b Point) {
+	y := a.y
+	if a.x < b.x {
+		if a.y < b.y {
+			for x := a.x; x <= b.x; x++ {
+				field[Point{x, y}]++
+				y++
+			}
+		} else {
+			for x := a.x; x <= b.x; x++ {
+				field[Point{x, y}]++
+				y--
+			}
+		}
+	} else {
+		if a.y < b.y {
+			for x := a.x; x >= b.x; x-- {
+				field[Point{x, y}]++
+				y++
+			}
+		} else {
+			for x := a.x; x >= b.x; x-- {
+				field[Point{x, y}]++
+				y--
+			}
+		}
+	}
+}
+
+func fill_field(field map[Point]int, a Point, b Point, diagonal bool) {
 	if a.x == b.x {
 		if a.y < b.y {
 
@@ -37,6 +66,8 @@ func fill_field(field map[Point]int, a Point, b Point) {
 			}
 
 		}
+	} else if diagonal {
+		fill_field_diagonal(field, a, b)
 	}
 }
 
@@ -52,7 +83,8 @@ func main() {
 	}
 	defer file.Close()
 
-	field := make(map[Point]int)
+	fieldp1 := make(map[Point]int)
+	fieldp2 := make(map[Point]int)
 
 	scanner := bufio.NewScanner(file)
 	for scanner.Scan() {
@@ -61,11 +93,20 @@ func main() {
 		endstr := strings.Split(parts[1], ",")
 		start := Point{clean_atoi(startstr[0]), clean_atoi(startstr[1])}
 		end := Point{clean_atoi(endstr[0]), clean_atoi(endstr[1])}
-		fill_field(field, start, end)
+		fill_field(fieldp1, start, end, false)
+		fill_field(fieldp2, start, end, true)
 	}
 
 	count := 0
-	for _, p := range field {
+	for _, p := range fieldp1 {
+		if p > 1 {
+			count++
+		}
+	}
+	fmt.Println(count)
+
+	count = 0
+	for _, p := range fieldp2 {
 		if p > 1 {
 			count++
 		}
--- 5/runner_test.go
+++ 5/runner_test.go
@@ -16,14 +16,14 @@ func Test_clean_atoi(t *testing.T) {
 func Test_fill_field_x(t *testing.T) {
 	expected := map[Point]int{Point{1, 1}: 1, Point{1, 2}: 1, Point{1, 3}: 1}
 	field := make(map[Point]int)
-	fill_field(field, Point{1, 1}, Point{1, 3})
+	fill_field(field, Point{1, 1}, Point{1, 3}, false)
 
 	if !reflect.DeepEqual(field, expected) {
 		t.Errorf("Expected %v got %v", expected, field)
 	}
 
 	field = make(map[Point]int)
-	fill_field(field, Point{1, 3}, Point{1, 1})
+	fill_field(field, Point{1, 3}, Point{1, 1}, false)
 	if !reflect.DeepEqual(field, expected) {
 		t.Errorf("Expected %v got %v", expected, field)
 	}
@@ -32,14 +32,30 @@ func Test_fill_field_x(t *testing.T) {
 func Test_fill_field_y(t *testing.T) {
 	expected := map[Point]int{Point{1, 1}: 1, Point{2, 1}: 1, Point{3, 1}: 1}
 	field := make(map[Point]int)
-	fill_field(field, Point{1, 1}, Point{3, 1})
+	fill_field(field, Point{1, 1}, Point{3, 1}, false)
 
 	if !reflect.DeepEqual(field, expected) {
 		t.Errorf("Expected %v got %v", expected, field)
 	}
 
 	field = make(map[Point]int)
-	fill_field(field, Point{3, 1}, Point{1, 1})
+	fill_field(field, Point{3, 1}, Point{1, 1}, false)
+	if !reflect.DeepEqual(field, expected) {
+		t.Errorf("Expected %v got %v", expected, field)
+	}
+}
+
+func Test_fill_field_diagonal(t *testing.T) {
+	expected := map[Point]int{Point{5, 5}: 1, Point{6, 4}: 1, Point{7, 3}: 1, Point{8, 2}: 1}
+	field := make(map[Point]int)
+
+	fill_field_diagonal(field, Point{5, 5}, Point{8, 2})
+	if !reflect.DeepEqual(field, expected) {
+		t.Errorf("Expected %v got %v", expected, field)
+	}
+
+	field = make(map[Point]int)
+	fill_field_diagonal(field, Point{8, 2}, Point{5, 5})
 	if !reflect.DeepEqual(field, expected) {
 		t.Errorf("Expected %v got %v", expected, field)
 	}
