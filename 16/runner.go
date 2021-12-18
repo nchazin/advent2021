e35bea6c8d9f9ca70bb63c63b61dbb9d7bc697f0
Neil Chazin <nchazin@salesforce.com>
2021-12-18 14:40:55 -0800

    finished day 16

--- 16/runner.go
+++ 16/runner.go
@@ -4,17 +4,20 @@ import (
 	"fmt"
 	"io/ioutil"
 	"log"
+	"math"
 	"strconv"
 )
 
 var LITERAL_ID = int(4)
 
 type Packet struct {
-	literal int
+	value   uint64
 	id      int
 	version int
 	length  int
 }
+
+var total_version int
 
 var codewheel = map[byte][]byte{
 	'0': []byte{'0', '0', '0', '0'},
@@ -35,6 +38,16 @@ var codewheel = map[byte][]byte{
 	'F': []byte{'1', '1', '1', '1'},
 }
 
+var operators = map[int]string{
+	0: "sum",
+	1: "product",
+	2: "min",
+	3: "max",
+	5: "gt",
+	6: "lt",
+	7: "equals",
+}
+
 func input_to_bits(input []byte) []byte {
 	output := make([]byte, 0)
 	for _, c := range input {
@@ -53,8 +66,7 @@ func packet_id(input []byte) int {
 	return int(id)
 }
 
-func extract_literal(input []byte) (int, int) {
-	fmt.Println("literal")
+func extract_literal(input []byte) (uint64, int) {
 	literal := make([]byte, 0)
 	lasti := 0
 	for i := 6; i+4 < len(input); i += 5 {
@@ -65,9 +77,8 @@ func extract_literal(input []byte) (int, int) {
 			break
 		}
 	}
-	literal_int, _ := strconv.ParseInt(string(literal), 2, 32)
-	//fmt.Println("Next i:", next_i)
-	return int(literal_int), lasti
+	literal_int, _ := strconv.ParseUint(string(literal), 2, 64)
+	return uint64(literal_int), lasti
 }
 
 func length_type(input []byte) int {
@@ -78,179 +89,162 @@ func length_type(input []byte) int {
 	}
 }
 
-func extract_length_packet(input []byte) ([]Packet, int) {
+func extract_length_packet(input []byte, packet *Packet) int {
 	packets := make([]Packet, 0)
 	length, _ := strconv.ParseInt(string(input[7:22]), 2, 32)
-	fmt.Println("Length: ", length)
 	i := 22
 	for i < int(22+length) {
-		newpackets, n := parse_packet(input[i:])
-		packets = append(packets, newpackets...)
+		newpacket, n := parse_packet(input[i:])
+		packets = append(packets, newpacket)
 		i += n
 	}
-	return packets, int(i)
+	packet.value = do_operator(packet_id(input), packets)
+	return int(i)
 
 }
 
-func extract_count_packet(input []byte) ([]Packet, int) {
+func extract_count_packet(input []byte, packet *Packet) int {
 	packets := make([]Packet, 0)
-	//	fmt.Println(string(input))
-	//	fmt.Println(string(input[7:18]))
 	count, _ := strconv.ParseInt(string(input[7:18]), 2, 32)
 	cur_point := 18
 	for i := 0; i < int(count); i++ {
-		fmt.Printf("WOrking on %d of %d at %d\n", i, count, cur_point)
 		packet, n := parse_packet(input[cur_point:])
-		packets = append(packets, packet...)
+		packets = append(packets, packet)
 		cur_point += n
 	}
-	fmt.Println("Curpoint is now: ", cur_point)
-	return packets, cur_point
+	packet.value = do_operator(packet_id(input), packets)
+
+	return cur_point
 }
 
-func extract_operator(input []byte) ([]Packet, int) {
+func extract_operator(input []byte, packet *Packet) int {
 	length_type := length_type(input)
-	fmt.Println("length type: ", length_type)
 	if length_type == 0 {
-		return extract_length_packet(input)
+		return extract_length_packet(input, packet)
 	} else {
-		return extract_count_packet(input)
+		return extract_count_packet(input, packet)
 	}
 }
 
-/*
-000 000 100000110101 1001001010111011100101001011100101010110110100
+func do_operator(operator int, packets []Packet) uint64 {
+	//fmt.Printf("%d %s - %v\n", operator, operators[operator], packets)
+	/*
+		Packets with type ID 0 are sum packets - their value is the sum of the values of their sub-packets. If they only have a single sub-packet, their value is the value of the sub-packet.
+		Packets with type ID 1 are product packets - their value is the result of multiplying together the values of their sub-packets. If they only have a single sub-packet, their value is the value of the sub-packet.
+		Packets with type ID 2 are minimum packets - their value is the minimum of the values of their sub-packets.
+		Packets with type ID 3 are maximum packets - their value is the maximum of the values of their sub-packets.
+		Packets with type ID 5 are greater than packets - their value is 1 if the value of the first sub-packet is greater than the value of the second sub-packet; otherwise, their value is 0. These packets always have exactly two sub-packets.
+		Packets with type ID 6 are less than packets - their value is 1 if the value of the first sub-packet is less than the value of the second sub-packet; otherwise, their value is 0. These packets always have exactly two sub-packets.
+		Packets with type ID 7 are equal to packets - their value is 1 if the value of the first sub-packet is equal to the value of the second sub-packet; otherwise, their value is 0. These packets always have exactly two sub-packets.
+	*/
+	switch operator {
+	case 0:
+		return do_sum(packets)
+	case 1:
+		return do_product(packets)
+	case 2:
+		return do_min(packets)
+	case 3:
+		return do_max(packets)
+	case 5:
+		return do_gt(packets)
+	case 6:
+		return do_lt(packets)
+	case 7:
+		return do_eq(packets)
+	}
+	return 0
+}
 
-100 100 1 0101 1 1011 1 0010 1 0010 1 1100 1 0101 0 1101
-10100110000000001011110
+func do_sum(packets []Packet) uint64 {
+	sum := uint64(0)
+	for _, p := range packets {
+		sum += p.value
+	}
+	return sum
+}
 
-101 001 1 00000000010
+func do_product(packets []Packet) uint64 {
+	product := uint64(1)
+	for _, p := range packets {
+		product *= p.value
+	}
+	return product
+}
 
-1111001001110001111101110111010110000101101011 (0 of 2)
+func do_min(packets []Packet) uint64 {
+	min := uint64(math.MaxUint64)
+	for _, p := range packets {
+		if p.value < min {
+			min = p.value
+		}
+	}
+	return min
+}
 
-111 100 1 0011 1 0001 1 1110 1 1101 1 1010 1 1000 0 1011
+func do_max(packets []Packet) uint64 {
+	max := uint64(0)
+	for _, p := range packets {
+		if p.value > max {
+			max = p.value
+		}
+	}
+	return max
+}
 
-01011000000000001010100 1 of 2
-010 110 0 000000000101010 010100 111101001000011111100111101001000011
-*/
+func do_gt(packets []Packet) uint64 {
+	if packets[0].value > packets[1].value {
+		return 1
+	}
+	return 0
+}
 
-func parse_packet(input []byte) ([]Packet, int) {
-	//	fmt.Println(string(input))
-	//fmt.Println("Data length: ", len(input))
-	fmt.Println("Data start: ", string(input[0:64]))
+func do_lt(packets []Packet) uint64 {
+	if packets[0].value < packets[1].value {
+		return 1
+	}
+	return 0
+}
+
+func do_eq(packets []Packet) uint64 {
+	if packets[0].value == packets[1].value {
+		return 1
+	}
+	return 0
+}
+
+func parse_packet(input []byte) (Packet, int) {
 	version := version(input)
+	total_version += version
 	next_index := 0
 	id := packet_id(input)
 	packet := Packet{}
 	packet.version = version
 	packet.id = id
-	fmt.Println("packet version:", version)
 	packets := make([]Packet, 0)
 	if id == LITERAL_ID {
 		val, n := extract_literal(input)
-		packet.literal = val
+		packet.value = val
 		packet.length = n
 		next_index = n
 		packets = append(packets, packet)
 	} else {
-		fmt.Println("Operator on")
-		command_packets, n := extract_operator(input)
-		packets = append(packets, packet)
-		packets = append(packets, command_packets...)
+		n := extract_operator(input, &packet)
 		next_index = n
 	}
-	return packets, next_index
+	return packet, next_index
 }
 
 func do_main(data []byte) {
 	op, _ := parse_packet(data)
-	osum := 0
-	for _, pp := range op {
-		fmt.Println("version: ", pp.version, " id:", pp.id, " value:", pp.literal)
-		osum += pp.version
-	}
-	fmt.Println(osum)
+	fmt.Println("value: ", op.value)
+	fmt.Println("versions: ", total_version)
 }
 
 func main() {
-	/*for _, c := range []byte{'0', '1', '2', '3', '4', '5', '6', '7', '8', '9', 'A', 'B', 'C', 'D', 'E', 'F'} {
-		fmt.Printf("%c -> %s\n", c, codewheel[c])
-	}*/
-
-	/* SKIP FOR NOW...
-	input := input_to_bits([]byte("D2FE28D2FE28"))
-	cur_index := 0
-	packets := make([]Packet, 0)
-	i := 0
-	for cur_index < len(input) {
-		p, n := parse_packet(input[cur_index:])
-		packets = append(packets, p...)
-		cur_index += n
-		if i > 5 {
-			break
-		}
-		i++
-	}
-	for _, p := range packets {
-		fmt.Println(p)
-	}
-	*/
-
 	data, err := ioutil.ReadFile("input.txt")
 	if err != nil {
 		log.Fatal(err)
 	}
-	sumbits := "00000110101"
-	ss, _ := strconv.ParseInt(sumbits, 2, 32)
-	fmt.Println(ss)
-
-	fmt.Println()
 	do_main(input_to_bits(data))
-	return
-
-	fmt.Println("EE00D40C823060")
-	ip := input_to_bits([]byte("EE00D40C823060"))
-	fmt.Println("Len type: ", length_type(ip))
-	fmt.Println(string(ip))
-	fmt.Println("-----")
-	a, acount := extract_count_packet(ip)
-	fmt.Println("-", a)
-	fmt.Println("------> acount: ", acount, " ... ", len(ip))
-	for _, pp := range a {
-		fmt.Println("version: ", pp.version, " value:", pp.literal)
-	}
-
-	fmt.Println("38006F45291200")
-	ip = input_to_bits([]byte("38006F45291200"))
-	fmt.Println("Len type: ", length_type(ip))
-	x, xcount := extract_length_packet(ip)
-	fmt.Println("------> xcount: ", xcount, " ... ", len(ip))
-
-	for _, pp := range x {
-		fmt.Println("version: ", pp.version, " value:", pp.literal)
-	}
-
-	ip = input_to_bits([]byte("A0016C880162017C3686B18A3D4780"))
-	xyz, _ := parse_packet(ip)
-	sum := 0
-	for _, pp := range xyz {
-		fmt.Println("version: ", pp.version, " id:", pp.id, " value:", pp.literal)
-		sum += pp.version
-	}
-	fmt.Println(sum)
-
-	ip = input_to_bits([]byte("020D64AEE52E55B"))
-	fmt.Printf("Version: %d\n", version(ip))
-	fmt.Printf("Id: %d\n", packet_id(ip))
-	fmt.Printf("Lentght Type: %d\n", length_type((ip)))
-	//y, _ := extract_length_packet(ip)
-
-	/*
-		output := input_to_bits([]byte("ABC123"))
-		fmt.Printf("%s\n", string(output))
-		fmt.Printf("Version: %d\n", version(input_to_bits([]byte("D2FE28"))))
-		fmt.Printf("Id: %d\n", packet_id(input_to_bits([]byte("D2FE28"))))
-		fmt.Printf("literal: %d\n", extract_literal(input_to_bits([]byte("D2FE28"))))
-	*/
-
 }
