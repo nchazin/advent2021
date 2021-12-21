3fbb35294c646d968fe9375f42163a9295578631
Neil Chazin <nchazin@salesforce.com>
2021-12-21 14:12:52 -0800

    Solved part 2 of day 21

--- 21/runner.go
+++ 21/runner.go
@@ -36,6 +36,62 @@ func (p *Player) play(d *Die) bool {
 	return p.score >= 1000
 }
 
+func (p *Player) move(move int) {
+	for i := 0; i < move; i++ {
+		p.position++
+		if p.position > 10 {
+			p.position = 1
+		}
+	}
+	p.score += p.position
+}
+
+type GameState struct {
+	players [2]Player
+	count   int
+}
+
+// rolling a d3 3 times, this is  the frequency map out outcomes
+// from this set:
+// [3, 4, 5, 4, 5, 6, 5, 6, 7, 4, 5, 6, 5, 6, 7, 6, 7, 8, 5, 6, 7, 6, 7, 8, 7, 8, 9]
+var DiceOutcomes = map[int]int{3: 1, 4: 3, 5: 6, 6: 7, 7: 6, 8: 3, 9: 1}
+
+// this enumertes the rolls themselves, for ordering
+var possible_rolls = [7]int{3, 4, 5, 6, 7, 8, 9}
+
+func play_a_turn(g GameState, player int, universes uint64, wins *[2]uint64) {
+
+	for _, r := range possible_rolls {
+		/*if old_score != g.players[player].score {
+			fmt.Errorf("Score not equal!!!")
+			os.Exit(1)
+		}*/
+		newGameState := g
+		newGameState.players[player].move(r)
+		fanout := uint64(DiceOutcomes[r]) * universes
+
+		if newGameState.players[player].score >= 21 {
+			wins[player] += fanout
+		} else {
+			play_a_turn(newGameState, (player+1)%2, fanout, wins)
+		}
+	}
+}
+
+func quantum_gameplay() {
+	g := GameState{}
+	/*
+		g.players[0].position = 4
+		g.players[1].position = 8
+	*/
+	g.players[0].position = 9
+	g.players[1].position = 3
+	var wins [2]uint64
+	play_a_turn(g, 0, 1, &wins)
+	fmt.Println("W1: ", uint64(wins[0]), " W2: ", uint64(wins[1]))
+	fmt.Println(wins)
+}
+
 func main() {
 
 	d := Die{1, 0, 100}
@@ -58,4 +114,6 @@ func main() {
 		}
 	}
 	fmt.Println(loser.score, d.count, loser.score*d.count)
+
+	quantum_gameplay()
 }
